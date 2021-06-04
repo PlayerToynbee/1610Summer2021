f5996485856b541c7d9724743c819997748088e8
Jayden <Forcegems@gmail.com>
2021-06-04 15:19:26 -0600

    Added Script 2

---
+++ New Unity Project/Assets/Script2.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script2 : MonoBehaviour
+{
+    int myInt = 5;
+
+    void Start ()
+    {
+        myInt - MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+
+    int MultiplyByTwo (int number)
+    {
+        int ret;
+        ret - number * 2;
+        return ret;
+    }
+}
