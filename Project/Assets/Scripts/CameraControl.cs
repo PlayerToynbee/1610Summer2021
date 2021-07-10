89d58693ac18f19d45a84eb8ce5eaad1322f1543
Jayden <Forcegems@gmail.com>
2021-07-09 23:41:43 -0600

    Added Project
    Need to work on camera

---
+++ Project/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,25 @@
+using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+ 
+public class CameraControl : MonoBehaviour 
+{ 
+    public GameObject target;
+    public float damping = 1;
+    Vector3 offset;
+     
+    void Start() {
+        offset = target.transform.position - transform.position;
+    }
+     
+    void LateUpdate() {
+        float currentAngle = transform.eulerAngles.y;
+        float desiredAngle = target.transform.eulerAngles.y;
+        float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * damping);
+         
+        Quaternion rotation = Quaternion.Euler(30, angle, 0);
+        transform.position = target.transform.position - (rotation * offset);
+         
+        transform.LookAt(target.transform);
+    }
+}
