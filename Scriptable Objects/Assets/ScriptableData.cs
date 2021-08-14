f3b1b7456537de546bf78e811e9bb6f2b135f6d5
Jayden <Forcegems@gmail.com>
2021-08-13 21:46:19 -0600

    Scriptable events

---
+++ Scriptable Objects/Assets/ScriptableData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class ScriptableData : ScriptableObject
+{
+    public int value;
+    public Sprite displayImage;
+    public string displayNumber;
+}
